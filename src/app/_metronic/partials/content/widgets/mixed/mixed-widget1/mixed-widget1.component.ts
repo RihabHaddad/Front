e0b3633f343374053864c23624f3af82104793d0
RihabHaddad <rihab.haddad@esprit.tn>
2023-08-29 19:36:45 +0100

    dr

--- src/app/_metronic/partials/content/widgets/mixed/mixed-widget1/mixed-widget1.component.ts
+++ src/app/_metronic/partials/content/widgets/mixed/mixed-widget1/mixed-widget1.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/pages/dashboard/DataService';
 import { KpiService } from 'src/app/pages/driver-behavior/KpiService';
 
 @Component({
@@ -8,21 +9,49 @@ import { KpiService } from 'src/app/pages/driver-behavior/KpiService';
 })
 export class MixedWidget1Component {
   @Input() color: string = '';
-  driverId = '1'; // ID du driver que vous souhaitez afficher
-  driverKPIs: any[] = []; // Tableau pour stocker les KPI du driver
+  driverId = '1';
+  driverKPIs: any[] = [];
   data: any[] = [];
   private dataSubscription: Subscription;
-  constructor(private kpiService: KpiService,) {}
+
+  constructor(private kpiService: KpiService, private dataService: DataService) {}
 
   ngOnInit(): void {
+    console.log('Component initialized');
     this.loadDriverKPIs();
+    this.loadData();
+  }
+
+  loadData() {
+    console.log('Loading data...');
+    this.dataSubscription = this.dataService.getDataFromSpark().subscribe(
+      response => {
+        console.log('Data loaded:', response);
+        this.data = response;
+      },
+      error => {
+        console.error('API request error:', error);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    console.log('Component destroyed');
+    if (this.dataSubscription) {
+      this.dataSubscription.unsubscribe();
+    }
+  }
+
+  trackByDriverId(index: number, item: any) {
+    return item.DriverId;
   }
 
   loadDriverKPIs(): void {
+    console.log('Loading driver KPIs...');
     this.kpiService.getDriverKPIs(this.driverId).subscribe(
       (data) => {
         console.log('Driver KPIs:', data);
-        this.driverKPIs = data; // Mettre à jour les KPIs du driver
+        this.driverKPIs = data;
       },
       (error) => {
         console.error('Error:', error);
